914f74124036e4c6c0ed274444133c5f25b207c2
Jens Peder Meldgaard <jpm@halfspace.io>
2025-01-07 15:46:01 +0100

    refactor: everything
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

---
+++ src/pymono/types.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass
+class Dependency:
+    name: str
+    is_workspace: bool
+
+
+@dataclass
+class Project:
+    name: str
+    version: str
+    path: Path
+    dependencies: list[Dependency]
