1eae39bd6471ff79eb301a4fefad95bbdc248dd7
Jens Peder Meldgaard <jpm@halfspace.io>
2025-01-07 16:04:03 +0100

    test: add func to c
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- packages/package-c/src/package_c/__init__.py
+++ packages/package-c/src/package_c/__init__.py
@@ -2,3 +2,8 @@ def main() -> None:
     msg = "Hello from package_c!" 
     print(msg)
     return msg
+
+def another_function() -> None:
+    msg = "Another function from package_c!"
+    print(msg)
+    return msg
